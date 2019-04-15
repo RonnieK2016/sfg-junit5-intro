7dcce61951da0ca2cf4dbec4ff611987ab59a74b
SergeA <ronnieo@gmail.com>
2019-04-15 18:00:29 -0400

    Initial Commit

---
+++ src/main/java/com/example/sfgtdd/sfgjunit5intro/SfgJunit5IntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgtdd.sfgjunit5intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJunit5IntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJunit5IntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sfgtdd/sfgjunit5intro/SfgJunit5IntroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sfgtdd.sfgjunit5intro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgJunit5IntroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
