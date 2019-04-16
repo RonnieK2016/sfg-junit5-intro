d52891ffc9835e994f4ec722180c12693527ba67
SergeA <ronnieo@gmail.com>
2019-04-15 21:43:27 -0400

    Added Set-Up method for GreetingTest

--- src/test/java/com/example/sfgtdd/sfgjunit5intro/app/GreetingTest.java
+++ src/test/java/com/example/sfgtdd/sfgjunit5intro/app/GreetingTest.java
@@ -1,20 +1,26 @@
 package com.example.sfgtdd.sfgjunit5intro.app;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         assertEquals("Hello World", greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
         assertEquals("Hello John", greeting.helloWorld("John"));
     }
 }
